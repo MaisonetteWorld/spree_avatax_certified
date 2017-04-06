fca9b7bfc94f926d5532adc05ad80e92b805940d
Michał Jóźwin <michal@itideas.pl>
2017-04-06 14:41:15 +0200

    MAIS-418 - update payment record with valid order total amount

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Payment.class_eval do
   self.state_machine.before_transition to: :completed, do: :avalara_finalize
+  self.state_machine.before_transition to: :processing, do: :avalara_update
   self.state_machine.after_transition to: :void, do: :cancel_avalara
 
   def avalara_tax_enabled?
@@ -8,6 +9,15 @@ Spree::Payment.class_eval do
 
   def cancel_avalara
     order.avalara_transaction.cancel_order unless order.avalara_transaction.nil?
+  end
+
+  def avalara_update
+    # without reload order was charged without tax amount
+    order.reload
+
+    if self.amount != order.total
+      self.update_attributes(amount: order.total)
+    end
   end
 
   def avalara_finalize
